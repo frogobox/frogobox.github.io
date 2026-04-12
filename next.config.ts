35c141bc7eedd163a304c83d557043bc153e4d0b
amirisback <faisalamircs@gmail.com>
2026-04-12 13:30:56 +0700

    feat: migrate project to Next.js with new app directory structure and assets

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from "next";
+import withSerwistInit from "@serwist/next";
+
+const withSerwist = withSerwistInit({
+  swSrc: "app/sw.ts",
+  swDest: "public/sw.js",
+  disable: process.env.NODE_ENV === "development",
+});
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default withSerwist(nextConfig);
